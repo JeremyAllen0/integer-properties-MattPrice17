123f474a1f52c7d3ef96fb112cdee6cfe90c2632
CSAllenISD <35275250+CSAllenISD@users.noreply.github.com>
2018-01-21 20:28:51 -0600

    Create integerProperties.swift

---
+++ integerProperties.swift
@@ -0,0 +1,45 @@
+// Allen ISD Computer Science Assignment
+// Integer Properties Project
+// John Williams
+// Computer Science I, Period 1
+// 2018.01.21
+
+
+/* This program will print many properties for an integer specified on the command line.
+   
+   Is the number even? (true/false)
+   Is the number odd? (true/false)
+   Is the number evenly divisible by three? (true/false)
+   Is the number evenly divisible by five? (true/false)
+   Is the number evenly divisible by seven? (true/false)
+
+   How many groups of ten are there? (integer value)
+   How many groups of hundred are there?  (integer value)
+
+   The additive inverse of the number: (integer value)
+   
+
+
+
+   Example output for:  ./integerProperties 1983
+
+   The number is even:
+   false
+   The number is odd:
+   true
+   The number is evenly divisible by three:
+   true
+   The number is evenly divisible by five:
+   false
+   The number is evenly divisible by seven:
+   false
+   How many groups of ten?
+   198
+   How many groups of hundred?
+   19
+   The additive inverse:
+   -1983
+   
+
+
+ */
